48008de50796e1338006c6ff9c484cb154d84f16
Serhii Horodilov <sgorodil@gmail.com>
2023-07-13 15:56:10 +0300

    Add members app models
    Add custom member models to handle application users.
    User model is extended with "team" foreign key (nullable).

--- happymeter/settings.py
+++ happymeter/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "members.MemberModel"
+
 AUTH_PASSWORD_VALIDATORS = []
 
 # Internationalization
