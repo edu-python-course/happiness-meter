ecd255a2b802e425bda12eaf4347fb42b7d5531e
Serhii Horodilov <sgorodil@gmail.com>
2023-07-14 05:25:51 +0300

    Add annotated happiness reports read-only serializer

--- reports/serializers.py
+++ reports/serializers.py
@@ -13,3 +13,18 @@ class HappinessReportModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = HappinessReportModel
         fields = "level", "reporter"
+
+
+class AnnotatedHappinessReportSerializer(serializers.Serializer):
+    """Annotated happiness reports serializer"""
+
+    date = serializers.DateField()
+    team = serializers.CharField()
+    level = serializers.FloatField()
+    prev = serializers.FloatField()
+
+    def create(self, validated_data):
+        raise NotImplementedError("Operation is restricted")
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError("Operation is restricted")
