ea464e545c9defa36e25b5efa3cf619396460c77
Serhii Horodilov <sgorodil@gmail.com>
2023-07-14 03:58:52 +0300

    Update member model serializer
    Implemented user creation and update operations.

--- members/serializers.py
+++ members/serializers.py
@@ -16,13 +16,45 @@ class MemberModelSerializer(serializers.ModelSerializer):
         required=False,
         write_only=True
     )
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = MemberModel
         fields = (
-            "pk", "username", "first_name", "last_name",
-            "team"
+            "pk", "username", "first_name", "last_name", "email",
+            "team", "password", "confirm_password"
         )
+
+    def validate(self, data):
+        """Validate serialized data"""
+
+        if data.get("password") != data.get("confirm_password"):
+            error_message = "Password does not match"
+            errors = {
+                "password": error_message,
+                "confirm_password": error_message
+            }
+            raise serializers.ValidationError(errors)
+
+        return super().validate(data)
+
+    def create(self, validated_data) -> MemberModel:
+        """Create a member instance"""
+
+        del validated_data["confirm_password"]
+
+        return MemberModel.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data) -> MemberModel:
+        """Update a member instance"""
+
+        instance = super().update(instance, validated_data)
+
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
+
+        return instance
 
 
 class TeamModelSerializer(serializers.ModelSerializer):
