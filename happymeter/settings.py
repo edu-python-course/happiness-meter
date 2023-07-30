86d91abc97756496f84a17fe308daea6e1c1b3ab
Serhii Horodilov <sgorodil@gmail.com>
2023-07-30 11:44:43 +0300

    Change default DB backend

--- happymeter/settings.py
+++ happymeter/settings.py
@@ -70,6 +70,10 @@ WSGI_APPLICATION = "happymeter.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "postgresql": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "happymeter",
         "HOST": os.environ.get("PGHOST", "localhost"),
